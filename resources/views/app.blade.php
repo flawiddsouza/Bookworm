37f9368fad8364f79dc9b81e35b99247d41f72ff
Flawid DSouza <me@flawiddsouza.com>
2021-03-17 21:53:51 +0530

    cleaning up files generated by npx apply laravel:vite

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <title>Laravel</title>
-  @vite
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bookworm</title>
+    @vite
 </head>
 <body class="antialiased">
-  <div id="app"></div>
+    <div id="app"></div>
 </body>
 </html>
