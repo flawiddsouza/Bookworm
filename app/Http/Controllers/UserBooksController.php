14e9b272b1b34a6751258943f8c92f85c12624da
Flawid DSouza <me@flawiddsouza.com>
2021-03-23 18:29:47 +0530

    feat: Home > Order by user_books.completed_reading when status = READ

--- app/Http/Controllers/UserBooksController.php
+++ app/Http/Controllers/UserBooksController.php
@@ -12,33 +12,41 @@ class UserBooksController extends Controller
     {
         $sqlDateFormat = env('SQL_DATE_FORMAT');
 
+        $paginator = UserBook::selectRaw("
+            user_books.id,
+            CASE WHEN series.name IS NOT NULL THEN CONCAT(books.name, ' (', series.name, ' #', book_series.index, ')') ELSE books.name END as book,
+            book_types.name as book_type,
+            books.cover_image_url,
+            string_agg(
+                concat(
+                    authors.name,
+                    CASE WHEN book_authors.role IS NOT NULL THEN CONCAT(' (', book_authors.role, ')') ELSE '' END
+                ),
+                ', '
+            ) as author,
+            to_char(user_books.started_reading, '$sqlDateFormat') as started_reading,
+            to_char(user_books.completed_reading, '$sqlDateFormat') as completed_reading
+        ")
+        ->join('books', 'books.id', 'user_books.book_id')
+        ->join('book_types', 'book_types.id', 'books.book_type_id')
+        ->leftJoin('book_authors', 'book_authors.book_id', 'books.id')
+        ->leftJoin('authors', 'authors.id', 'book_authors.author_id')
+        ->leftJoin('book_series', 'book_series.book_id', 'books.id')
+        ->leftJoin('series', 'series.id', 'book_series.series_id')
+        ->where('user_books.status', $request->status)
+        ->where('user_books.user_id', Auth::id())
+        ->groupBy('user_books.id', 'series.id', 'books.id', 'book_series.id', 'book_types.id');
+
+        if($request->status === 'READ') {
+            $paginator = $paginator->orderBy('user_books.completed_reading', 'DESC');
+        } else {
+            $paginator = $paginator->orderBy('books.updated_at', 'DESC');
+        }
+
+        $paginator = $paginator->paginate(50);
+
         return [
-            'paginator' => UserBook::selectRaw("
-                user_books.id,
-                CASE WHEN series.name IS NOT NULL THEN CONCAT(books.name, ' (', series.name, ' #', book_series.index, ')') ELSE books.name END as book,
-                book_types.name as book_type,
-                books.cover_image_url,
-                string_agg(
-                    concat(
-                        authors.name,
-                        CASE WHEN book_authors.role IS NOT NULL THEN CONCAT(' (', book_authors.role, ')') ELSE '' END
-                    ),
-                    ', '
-                ) as author,
-                to_char(user_books.started_reading, '$sqlDateFormat') as started_reading,
-                to_char(user_books.completed_reading, '$sqlDateFormat') as completed_reading
-            ")
-            ->join('books', 'books.id', 'user_books.book_id')
-            ->join('book_types', 'book_types.id', 'books.book_type_id')
-            ->leftJoin('book_authors', 'book_authors.book_id', 'books.id')
-            ->leftJoin('authors', 'authors.id', 'book_authors.author_id')
-            ->leftJoin('book_series', 'book_series.book_id', 'books.id')
-            ->leftJoin('series', 'series.id', 'book_series.series_id')
-            ->where('user_books.status', $request->status)
-            ->where('user_books.user_id', Auth::id())
-            ->groupBy('user_books.id', 'series.id', 'books.id', 'book_series.id', 'book_types.id')
-            ->orderBy('books.updated_at', 'DESC')
-            ->paginate(50),
+            'paginator' => $paginator,
             'unfiltered_total' => UserBook::where('user_books.status', $request->status)
             ->where('user_books.user_id', Auth::id())
             ->count()
